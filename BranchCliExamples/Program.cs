4dfdb41aa319ce4c832344a757e0bed7c48c2187
Muhangi Charlie Shane Baine <183634683+ShaneMuhangi@users.noreply.github.com>
2024-10-07 11:45:13 +0100

    Add project files.

---
+++ BranchCliExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LearnCSharp;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the company's Revenue for the the year");
+        int revenue = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the company's Expenditure for the year");
+        int expenditure = int.Parse(Console.ReadLine());
+        int profit = revenue - expenditure;
+
+        if (revenue > expenditure)
+            Console.WriteLine("Company has a profit of " + profit);
+        else if (revenue < expenditure)
+            Console.WriteLine("Company has a loss of " + profit);
+        else
+            Console.WriteLine("Company has a break even of " + profit);
+
+
+        Console.WriteLine("Please hit enter to exit");
+        Console.ReadLine();
+    }
+}
---
+++ BranchCliExamples/readme.md
@@ -0,0 +1,3 @@
+
+# Branching Examples
+
